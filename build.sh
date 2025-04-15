a57fd05b0df664109964780c348eb7840d6467c1
Bruce Mutemula <mutemulaeb51@gmail.com>
2025-04-15 14:02:01 +0200

    Update build.sh to fix auth tables during deployment

--- build.sh
+++ build.sh
@@ -8,5 +8,11 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input
 
-# Apply database migrations
+# Apply database migrations - run auth migrations first
+python manage.py migrate auth --fake-initial
+python manage.py migrate contenttypes --fake-initial
+python manage.py migrate admin --fake-initial
+python manage.py migrate sessions --fake-initial
+
+# Then run all migrations
 python manage.py migrate
